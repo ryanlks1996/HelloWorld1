6e4411084eae12e48073c8eff9a1f1116cc689f7
Ryan Leong <leongks-wa15@student.tarc.edu.my>
2017-10-16 13:34:55 +0800

    Rejected commit: Default

---
+++ app/src/main/java/my/edu/taruc/helloworld/MainActivity.java
@@ -0,0 +1,42 @@
+package my.edu.taruc.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+
+    //refer to any UI components
+    public void displayName(View v){
+
+        TextView textViewName;
+        EditText editTextName;
+        EditText editTextAge;
+
+
+        textViewName = (TextView) findViewById(R.id.textViewName);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextAge = (EditText) findViewById(R.id.editTextAge);
+        int age = Integer.parseInt(editTextAge.getText().toString());
+
+        textViewName.setText(getString(R.string.greeting) + editTextName.getText()+", you will be " + age + " in 2018");
+
+    }
+
+    public void clearText(View v){
+
+        TextView textViewName;
+
+        textViewName = (TextView) findViewById(R.id.textViewName);
+        textViewName.setText(" ");
+    }
+}
